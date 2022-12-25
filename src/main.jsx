27eb62981faf1cdfc759f819e23b07db16e54834
nicolasjandre <nicolasjandre@live.com>
2022-12-25 19:18:11 -0300

    solving bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Routes } from './Routes'
+import { Routes } from './routes'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import { AuthProvider } from './hooks/auth'
